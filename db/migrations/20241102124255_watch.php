67e43aee6e43a2c9302a492b4931558c098ed7ed
Zhenliang Yao <ucabz79@ucl.ac.uk>
2024-11-02 12:48:57 +0000

    Changes

---
+++ db/migrations/20241102124255_watch.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Watch extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up (): void
+    {
+        $sql = <<<EOD
+        CREATE TABLE IF NOT EXISTS `watch` (
+            `watch_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `buyer_id` INT NOT NULL,
+            `auction_item_id` INT NOT NULL,
+            `add_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        EOD;
+
+    }
+}
