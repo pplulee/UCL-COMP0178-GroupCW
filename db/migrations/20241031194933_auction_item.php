e7544a24368b32270e58896bad002de6d67e9bb3
pplulee <hi@pplulee.me>
2024-11-02 11:14:42 +0000

    fix: auction item table

--- db/migrations/20241031194933_auction_item.php
+++ db/migrations/20241031194933_auction_item.php
@@ -17,11 +17,11 @@ final class AuctionItem extends AbstractMigration
             `Description` TEXT NOT NULL,
             `StartingPrice` DECIMAL(10, 2) NOT NULL,
             `ReservePrice` DECIMAL(10, 2),
-          //`CurrentBidPrice` DECIMAL(10, 2) DEFAULT 0.00,
-          //`BidIncrement` DECIMAL(10, 2) DEFAULT 1.00,
+            `CurrentBidPrice` DECIMAL(10, 2) DEFAULT 0.00,
+            `BidIncrement` DECIMAL(10, 2) DEFAULT 1.00,
             `StartingDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `EndDate` DATETIME NOT NULL TIMESTAMP,
-          //`Status` ENUM('active', 'closed', 'cancelled')
+            `EndDate` DATETIME NOT NULL,
+            `Status` ENUM('active', 'closed', 'cancelled')
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         EOD;
 
